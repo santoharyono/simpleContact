dc6d4c08379a0dca2de77cce2f5903603e9e21a7
santo <santoharyono@gmail.com>
2020-06-24 19:23:03 +0700

    test component

--- src/testing/component.spec.js
+++ src/testing/component.spec.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import {CustomButton} from '../components/molecules';
+import {
+  CustomButton,
+  CustomTextInput,
+  RoundButton,
+} from '../components/molecules';
 
 describe('Render customButton', () => {
   it('render customButton correctly', () => {
     const CustomButtonComponent = renderer.create(<CustomButton />).toJSON();
     expect(CustomButtonComponent).toMatchSnapshot();
+  });
+});
+
+describe('Render customTextInput', () => {
+  it('render customTextInput correctly', () => {
+    const CustomTextInputComponent = renderer
+      .create(<CustomTextInput />)
+      .toJSON();
+    expect(CustomTextInputComponent).toMatchSnapshot();
+  });
+});
+
+describe('Render roundButton', () => {
+  it('render roundButton correctly', () => {
+    const RoundButtonComponent = renderer.create(<RoundButton />).toJSON();
+    expect(RoundButtonComponent).toMatchSnapshot();
   });
 });
